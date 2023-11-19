6fa8f09cefc05d04828e54525df9b534343048ac
Sean Behan <codebam@riseup.net>
2023-11-19 17:50:38 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Ai } from '@cloudflare/ai';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	AI: any;
 }
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const ai = new Ai(env.AI);
+		const searchparams = new URL(request.url).searchParams;
+		const q = searchparams.get('q');
+		const content = q;
+		let chat = {
+			messages: [
+				{
+					role: 'user',
+					content: content,
+				},
+			],
+			stream: true,
+		};
+		const stream = await ai.run('@cf/meta/llama-2-7b-chat-fp16', chat);
+		return new Response(stream, {
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+				'Content-Type': 'text/event-stream',
+			},
+		});
 	},
 };
